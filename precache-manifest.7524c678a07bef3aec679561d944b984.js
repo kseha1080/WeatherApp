8000ff78db5352414a22db80c207b43d14c9b3f6
Seungha <seungha.kong@allegiantair.com>
2019-11-24 19:31:46 -0500

    Updates

---
+++ precache-manifest.7524c678a07bef3aec679561d944b984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d059557ad09a48846a88b7a340676659",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "432e435f812215ee8a5f",
+    "url": "/WeatherApp/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "432e435f812215ee8a5f",
+    "url": "/WeatherApp/static/js/2.64f4032a.chunk.js"
+  },
+  {
+    "revision": "05de728442bac6970e75",
+    "url": "/WeatherApp/static/js/main.6b36f887.chunk.js"
+  },
+  {
+    "revision": "88d8927b32027f437c05",
+    "url": "/WeatherApp/static/js/runtime-main.373f76ee.js"
+  }
+]);
